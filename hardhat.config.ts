ccd7eac9cdbef8edfb92cfb92ce4a03b049b8daf
Ronan Sandford <wighawag@gmail.com>
2021-02-21 09:21:28 +0000

    Initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,61 @@
+import 'dotenv/config';
+import {HardhatUserConfig} from 'hardhat/types';
+import 'hardhat-deploy';
+import 'hardhat-deploy-ethers';
+import 'hardhat-gas-reporter';
+import 'hardhat-typechain';
+import 'solidity-coverage';
+import {node_url, accounts} from './utils/network';
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: '0.7.1',
+  },
+  namedAccounts: {
+    deployer: 0,
+  },
+  networks: {
+    hardhat: {
+      accounts: accounts(),
+    },
+    localhost: {
+      url: 'http://localhost:8545',
+      accounts: accounts(),
+    },
+    mainnet: {
+      url: node_url('mainnet'),
+      accounts: accounts('mainnet'),
+    },
+    rinkeby: {
+      url: node_url('rinkeby'),
+      accounts: accounts('rinkeby'),
+    },
+    kovan: {
+      url: node_url('kovan'),
+      accounts: accounts('kovan'),
+    },
+    staging: {
+      url: node_url('kovan'),
+      accounts: accounts('kovan'),
+    },
+  },
+  paths: {
+    sources: 'src',
+  },
+  gasReporter: {
+    currency: 'USD',
+    gasPrice: 100,
+    enabled: process.env.REPORT_GAS ? true : false,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    maxMethodDiff: 10,
+  },
+  typechain: {
+    outDir: 'typechain',
+    target: 'ethers-v5',
+  },
+  mocha: {
+    timeout: 0,
+  },
+};
+
+export default config;
